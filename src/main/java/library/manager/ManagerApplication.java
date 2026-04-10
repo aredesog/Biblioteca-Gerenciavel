a60421927ba14924274eb35b36c4ee29fc6fb27c
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2026-04-10 17:13:22 -0300

    feat: configure PostgreSQL datasource and update application properties; implement CommandLineRunner in ManagerApplication

--- src/main/java/library/manager/ManagerApplication.java
+++ src/main/java/library/manager/ManagerApplication.java
@@ -6,9 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManagerApplication {
+public class ManagerApplication implements CommandLineRunner {
 
 	public static void main  (String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal.main(args);
 	}
+}
